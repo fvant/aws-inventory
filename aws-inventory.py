f4c39e60e4de2200da5c6aa5587c88149630524f
Frank van Tol <frank.vantol@gmail.com>
2018-02-17 21:10:53 +0100

    Update aws-inventory.py

--- aws-inventory.py
+++ aws-inventory.py
@@ -25,7 +25,7 @@ def signal_handler(signal, frame):
 signal.signal(signal.SIGINT, signal_handler)
 
 # All the regions we should be scanning
-__regions = ['eu-west-1', 'eu-central-1', 'us-east-1', 'us-west-2','us-west-1','ap-southeast-1','ap-southeast-2','ap-northeast-1','sa-east-1']
+__regions = ['eu-west-1', 'eu-west-2','eu-west-3', 'eu-central-1', 'us-east-1', 'us-east-2','us-west-1','us-west-2','ca-central-1','ap-southeast-1','ap-southeast-2','ap-northeast-1','ap-northeast-2','ap-northeast-3','ap-south-1','sa-east-1']
 
 def get_property_func(key):
     """
