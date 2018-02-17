d015de34d724d5cc487145cd3a2e550637b75eb0
Frank van Tol <frank.vantol@nordcloud.com>
2018-02-17 22:45:21 +0100

    added try:

--- aws-inventory.py
+++ aws-inventory.py
@@ -12,11 +12,8 @@ import boto3
 import argparse
 import logging
 
-__version__ = "0.1.2"
+__version__ = "0.2"
 
-LOG = logging.getLogger('aws-inventory')
-LOG_FORMAT = (
-    '%(asctime)s - %(threadName)s - %(name)s - %(levelname)s - %(message)s')
 
 def signal_handler(signal, frame):
     print('\n\nYOU CAN HAZ CTRL+C!\n\n')
@@ -26,6 +23,9 @@ signal.signal(signal.SIGINT, signal_handler)
 
 # All the regions we should be scanning
 __regions = ['eu-west-1', 'eu-west-2','eu-west-3', 'eu-central-1', 'us-east-1', 'us-east-2','us-west-1','us-west-2','ca-central-1','ap-southeast-1','ap-southeast-2','ap-northeast-1','ap-northeast-2','ap-northeast-3','ap-south-1','sa-east-1']
+
+__verbose = False
+__nodefault = False
 
 def get_property_func(key):
     """
@@ -104,6 +104,8 @@ def filter_key(filter_args):
     return filter_instance
 
 def print_result( title, table, headers=None ):
+    if __verbose:
+        print title
     if table:
         print '\n=== %s ===\n' % title
         print tabulate(table, headers)
@@ -141,7 +143,6 @@ def get_options(input_args, headers=None):
 def list_ec2(region, input_args=""):
     headers = (
         'name',
-        'customer',
         'environment',
         'state',
         'ip',
@@ -151,45 +152,47 @@ def list_ec2(region, input_args=""):
     )
     sort_by, filter_by_kwargs = get_options(input_args, headers)
 
-    conn = boto3.client('ec2', region)
-    instances = conn.describe_instances()
-    instanc = [i for r in instances['Reservations'] for i in r['Instances']]
-    to_row = lambda x: (
+    try:
+      conn = boto3.client('ec2', region)
+      instances = conn.describe_instances()
+      instanc = [i for r in instances['Reservations'] for i in r['Instances']]
+      to_row = lambda x: (
         get_tags(x, 'Name'),
-        get_tags(x, 'Tenant'),
         get_tags(x, 'Environment'),
         x['State']['Name'],
         get_try(x, 'PublicIpAddress'),
         get_try(x, 'PrivateIpAddress'),
         x['LaunchTime'],  #.split('T', 2)[0],
         x['InstanceId'],
-    )
-    print_result("EC2 @ region '%s'" % region,
+      )
+      print_result("EC2 @  '%s'" % region,
         process_list(instanc, to_row=to_row, sort_by=sort_by, filter_by=filter_by_kwargs), headers)
+    except:
+      logging.error( "Unexpected error: %s", sys.exc_info() )
 
 
 def list_elb(region, input_args=""):
     headers = (
         'name',
-        'customer',
         'environment',
         'created_time',
     )
     sort_by, filter_by_kwargs = get_options(input_args, headers)
 
     # response = conn.describe_tags(LoadBalancerNames=[name])
+    try:
+      conn = boto3.client('elb', region)
+      instances = conn.describe_load_balancers()
 
-    conn = boto3.client('elb', region)
-    instances = conn.describe_load_balancers()
-
-    to_row = lambda x: (
+      to_row = lambda x: (
         x['LoadBalancerName'],
-        elb_get_tags(conn, x['LoadBalancerName'], 'Tenant'),
         elb_get_tags(conn, x['LoadBalancerName'], 'Environment'),
         x['CreatedTime']
-    )
-    print_result("EC2:ELB @ region '%s'" % region,
+      )
+      print_result("EC2:ELB @  '%s'" % region,
         process_list(instances['LoadBalancerDescriptions'], to_row=to_row, sort_by=sort_by, filter_by=filter_by_kwargs), headers )
+    except:
+      logging.error( "Unexpected error: %s", sys.exc_info() )
 
 def list_volume(region, input_args=""):
     headers = (
@@ -200,16 +203,19 @@ def list_volume(region, input_args=""):
     )
     sort_by, filter_by_kwargs = get_options(input_args, headers)
 
-    conn = boto3.client('ec2', region)
-    instances = conn.describe_volumes()
-    to_row = lambda x: (
+    try:
+      conn = boto3.client('ec2', region)
+      instances = conn.describe_volumes()
+      to_row = lambda x: (
         x['VolumeId'],
         x['Size'],
         x['State'],
         x['CreateTime'],
-    )
-    print_result("EC2:Volumes @ region '%s'" % region,
+      )
+      print_result("EC2:Volumes @  '%s'" % region,
         process_list(instances['Volumes'], to_row=to_row, sort_by=sort_by, filter_by=filter_by_kwargs), headers )
+    except:
+      logging.error( "Unexpected error: %s", sys.exc_info() )
 
 def list_elasticache(region, input_args=""):
     headers = (
@@ -221,37 +227,52 @@ def list_elasticache(region, input_args=""):
     )
     sort_by, filter_by_kwargs = get_options(input_args, headers)
 
-    conn = boto3.client('elasticache', region)
-    instances = conn.describe_cache_clusters()
-    to_row = lambda x: (
+    try:
+      conn = boto3.client('elasticache', region)
+      instances = conn.describe_cache_clusters()
+      to_row = lambda x: (
         x['CacheClusterId'],
         x['Engine'],
         x['CacheClusterStatus'],
         elasticache_get_tags(conn, 'arn:aws:elasticache:'+region+':781369435176:cluster:'+x['CacheClusterId'], 'Environment'),
         x['CacheClusterCreateTime']
-    )
-    print_result("ElastiCache @ region '%s'" % region,
+      )
+      print_result("ElastiCache @  '%s'" % region,
         process_list(instances['CacheClusters'], to_row=to_row, sort_by=sort_by, filter_by=filter_by_kwargs), headers )
+    except:
+      logging.error( "Unexpected error: %s", sys.exc_info() )
 
 def list_vpc(region, input_args=""):
     headers = (
         'id',
         'name',
+        'default',
         'environment',
         'cidr_block'
     )
     sort_by, filter_by_kwargs = get_options(input_args, headers)
 
-    conn = boto3.client('ec2', region)
-    instances = conn.describe_vpcs()
-    to_row = lambda x: (
+    if __nodefault:
+      filter="{'Name':'isDefault','Values':[False]}"
+
+    try:
+      conn = boto3.client('ec2', region)
+      if __nodefault:
+        instances = conn.describe_vpcs(Filters=[filter],)
+      else:
+        instances = conn.describe_vpcs()
+
+      to_row = lambda x: (
         x['VpcId'],
         get_tags(x,'Name'),
+        x['IsDefault'],
         get_tags(x,'Environment'),
         x['CidrBlock']
-    )
-    print_result("VPC @ region '%s'" % region,
+      )
+      print_result("VPC @  '%s'" % region,
         process_list(instances['Vpcs'], to_row=to_row, sort_by=sort_by, filter_by=filter_by_kwargs), headers )
+    except:
+      logging.error( "Unexpected error: %s", sys.exc_info() )
 
 def list_sg(region, input_args=""):
     headers = (
@@ -261,16 +282,19 @@ def list_sg(region, input_args=""):
     )
     sort_by, filter_by_kwargs = get_options(input_args, headers)
 
-    conn = boto3.client('ec2', region)
-    instances = conn.describe_security_groups()
-    to_row = lambda x: (
+    try:
+      conn = boto3.client('ec2', region)
+      instances = conn.describe_security_groups()
+      to_row = lambda x: (
         x['GroupId'],
         x['GroupName'],
         get_tags(x,'Name'),
         get_tags(x,'Environment')
-    )
-    print_result("EC2:SG @ region '%s'" % region,
+      )
+      print_result("EC2:SG @  '%s'" % region,
         process_list(instances['SecurityGroups'], to_row=to_row, sort_by=sort_by, filter_by=filter_by_kwargs), headers )
+    except:
+      logging.error( "Unexpected error: %s", sys.exc_info() )
 
 def list_dbss(region, input_args=""):
     headers = (
@@ -281,16 +305,20 @@ def list_dbss(region, input_args=""):
     )
     sort_by, filter_by_kwargs = get_options(input_args, headers)
 
-    conn = boto3.client('rds', region)
-    instances = conn.describe_db_snapshots()
-    to_row = lambda x: (
+    try:
+      conn = boto3.client('rds', region)
+      instances = conn.describe_db_snapshots()
+      to_row = lambda x: (
         x['DBSnapshotIdentifier'],
         x['DBInstanceIdentifier'],
         x['Engine'],
         x['SnapshotCreateTime']
-    )
-    print_result("RDS:Snapshots @ region '%s'" % region,
+      )
+      print_result("RDS:Snapshots @  '%s'" % region,
         process_list(instances['DBSnapshots'], to_row=to_row, sort_by=sort_by, filter_by=filter_by_kwargs), headers )
+    except:
+      logging.error( "Unexpected error: %s", sys.exc_info() )
+
 
 def list_ec2ss(region, input_args=""):
     headers = (
@@ -302,17 +330,20 @@ def list_ec2ss(region, input_args=""):
     )
     sort_by, filter_by_kwargs = get_options(input_args, headers)
 
-    conn = boto3.client('ec2', region)
-    instances = conn.describe_snapshots(Filters=[{ 'Name': 'owner-alias', 'Values' : ['self'] }])
-    to_row = lambda x: (
+    try:
+      conn = boto3.client('ec2', region)
+      instances = conn.describe_snapshots(Filters=[{ 'Name': 'owner-alias', 'Values' : ['self'] }])
+      to_row = lambda x: (
         x['SnapshotId'],
         x['State'],
         get_tags(x,'Environment'),
         x['VolumeSize'],
         x['StartTime']
-    )
-    print_result("EC2:Snapshots @ region '%s'" % region,
+      )
+      print_result("EC2:Snapshots @  '%s'" % region,
         process_list(instances['Snapshots'], to_row=to_row, sort_by=sort_by, filter_by=filter_by_kwargs), headers )
+    except:
+      logging.error( "Unexpected error: %s", sys.exc_info() )
 
 def list_ecss(region, input_args=""):
     headers = (
@@ -322,15 +353,18 @@ def list_ecss(region, input_args=""):
     )
     sort_by, filter_by_kwargs = get_options(input_args, headers)
 
-    conn = boto3.client('elasticache', region)
-    instances = conn.describe_snapshots()
-    to_row = lambda x: (
+    try:
+      conn = boto3.client('elasticache', region)
+      instances = conn.describe_snapshots()
+      to_row = lambda x: (
         x['SnapshotName'],
         x['SnapshotStatus'],
         x['SnapshotSource']
-    )
-    print_result("ElasticCache:Snapshots @ region '%s'" % region,
+      )
+      print_result("ElasticCache:Snapshots @  '%s'" % region,
         process_list(instances['Snapshots'], to_row=to_row, sort_by=sort_by, filter_by=filter_by_kwargs), headers )
+    except:
+      logging.error( "Unexpected error: %s", sys.exc_info() )
 
 
 
@@ -348,10 +382,13 @@ def list_rds(region, input_args=""):
     )
     sort_by, filter_by_kwargs = get_options(input_args, headers)
 
-    conn = boto3.client('rds', region)
-    instances = conn.describe_db_instances()
-    print_result( "RDS @ region '%s'" % region,
+    try:
+      conn = boto3.client('rds', region)
+      instances = conn.describe_db_instances()
+      print_result( "RDS @  '%s'" % region,
         process_list(instances['DBInstances'], to_row=to_row, sort_by=sort_by, filter_by=filter_by_kwargs), headers )
+    except:
+      logging.error( "Unexpected error: %s", sys.exc_info() )
 
 def list_s3(input_args=""):
     headers = (
@@ -365,14 +402,18 @@ def list_s3(input_args=""):
     conn = boto3.client('s3')
     instances = conn.list_buckets()
 
-    to_row = lambda x: (
+    try:
+      to_row = lambda x: (
         x['Name'],
         (conn.get_bucket_location(Bucket=x['Name'])['LocationConstraint'] or 'us-east-1').replace('EU','eu-west-1'),
         s3_get_tags(conn, x['Name'], 'Environment'),
         x['CreationDate']
-    )
-    print_result("S3 @ 'worldwide'",
+      )
+      print_result("S3 @ 'worldwide'",
         process_list(instances['Buckets'], to_row=to_row, sort_by=sort_by, filter_by=filter_by_kwargs), headers )
+    except:
+      logging.error( "Unexpected error: %s", sys.exc_info() )
+
 
 def _create_parser():
         parser = argparse.ArgumentParser(
@@ -381,7 +422,9 @@ def _create_parser():
         parser.add_argument("-V", "--version", action="version",
                             version="{}".format(__version__))
         parser.add_argument('-r', '--region', nargs='+', help='list of AWS Regions to collect from')
-        parser.add_argument('command', choices=['all', 'ec2','vpc','elb','rds','elasticache','sg','s3','colume','ec2ss','dbss'])
+        parser.add_argument('command', choices=['all', 'ec2','vpc','elb','rds','elasticache','sg','s3','volume','ec2ss','dbss'])
+        parser.add_argument('-v', '--verbose', action="store_true")
+        parser.add_argument('-nd', '--nodefault', help="skip defaut VPCs", action="store_true")
         return parser
 
 def main():
@@ -398,6 +441,14 @@ def main():
     else:
        regions = __regions
        
+    global __verbose
+    if _parsed_args.verbose:
+       __verbose = True
+
+    global __nodefault
+    if _parsed_args.nodefault:
+       __nodefault = True
+
     for _region in regions:
        globals()['list_'+_parsed_args.command](_region)
 
